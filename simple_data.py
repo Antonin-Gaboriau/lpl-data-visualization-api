3e184a94f6250b4733bee792c0bb5101c184622f
Antonin-Gaboriau <antonin.GABORIAU@etu.univ-amu.fr>
2018-04-11 16:13:13 +0200

    Smoothing improvement

--- simple_data.py
+++ simple_data.py
@@ -1,45 +1,37 @@
-from numpy import loadtxt, linspace, ones, convolve
+from numpy import loadtxt, linspace, arange
+from scipy import signal, interpolate
 from bokeh.plotting import figure, show
 from bokeh.layouts import gridplot
-from scipy import signal
 
 # Lecture des fichiers scv
 data4936A = loadtxt(open("data/SW_4936_speech_rate_A.csv", "rb"), delimiter="\t", skiprows=1)
 data4936B = loadtxt(open("data/SW_4936_speech_rate_B.csv", "rb"), delimiter="\t", skiprows=1)
-data2005A = loadtxt(open("data/SW_2005_speech_rate_A.csv", "rb"), delimiter="\t", skiprows=1)
-data2005B = loadtxt(open("data/SW_2005_speech_rate_B.csv", "rb"), delimiter="\t", skiprows=1)
 
-# Fonction de lissage des données
-def smooth(data):
+# Lissage des données
+def smooth(x,y):
+    s = {}
     b, a = signal.butter(10, 0.1)
     zi = signal.lfilter_zi(b, a)
-    z, _ = signal.lfilter(b, a, data, zi=zi*data[0])
+    z, _ = signal.lfilter(b, a, y, zi=zi*y[0])
     z2, _ = signal.lfilter(b, a, z, zi=zi*z[0])
-    return signal.filtfilt(b, a, data)
+    cs = interpolate.CubicSpline(x, signal.filtfilt(b, a, y))
+    s['x'] = arange(0, 300, 0.5)
+    s['y'] = cs(arange(0, 300, 0.5))
+    return s
 
-# Création graphique discussion 1
-graph1 = figure(width=1000, height=300, y_range=[0,3.5], title="Speech rate discussion 4936")
-graph1.line(data4936A[:,2], data4936A[:,1], legend="Locuteur A",
+s4936A = smooth(data4936A[:,2], data4936A[:,1])
+s4936B = smooth(data4936B[:,2], data4936B[:,1])
+
+# Création et affichage du graphique
+plot = figure(width=1000, height=300, y_range=[0,3.5], title="Speech rate discussion 4936")
+plot.line(data4936A[:,2], data4936A[:,1], legend="Locuteur A",
             line_width=1, color="blue", alpha=0.5, line_dash="10 4")
-graph1.line(data4936A[:,2], smooth(data4936A[:,1]), legend="Locuteur A lissé",
+plot.line(s4936A['x'], s4936A['y'], legend="Locuteur A lissé",
             line_width=2, color="blue")
-graph1.line(data4936B[:,2], data4936B[:,1], legend="Locuteur B",
+plot.line(data4936B[:,2], data4936B[:,1], legend="Locuteur B",
             line_width=1, color="red", alpha=0.5, line_dash="10 4")
-graph1.line(data4936B[:,2], smooth(data4936B[:,1]), legend="Locuteur B lissé",
+plot.line(s4936B['x'], s4936B['y'], legend="Locuteur B lissé",
             line_width=2, color="red")
 
-# Création graphique discussion 2
-graph2 = figure(width=1000, height=300, y_range=[0,3.5], title="Speech rate discussion 2005")
-graph2.line(data2005A[:,2], data2005A[:,1], legend="Locuteur A",
-            line_width=1, color="blue", alpha=0.5, line_dash="10 4")
-graph2.line(data2005A[:,2], smooth(data2005A[:,1]), legend="Locuteur A lissé",
-            line_width=2, color="blue")
-graph2.line(data2005B[:,2], data2005B[:,1], legend="Locuteur B",
-            line_width=1, color="red", alpha=0.5, line_dash="10 4")
-graph2.line(data2005B[:,2], smooth(data2005B[:,1]), legend="Locuteur B lissé",
-            line_width=2, color="red")
-
-# Affichage des grapgiques
-graphs = gridplot([[graph1],[graph2]])
-show(graphs)
+show(plot)
 
