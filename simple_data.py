0d50c404d72819328b2acddd6d13c47d872fe901
Antonin-Gaboriau <antonin.GABORIAU@etu.univ-amu.fr>
2018-04-12 17:07:49 +0200

    Smoothing method and code structure changes

--- simple_data.py
+++ simple_data.py
@@ -1,34 +1,40 @@
-from numpy import loadtxt, linspace, arange
+from numpy import *
 from scipy import signal, interpolate
 from bokeh.plotting import figure, show
 from bokeh.layouts import gridplot
 
-# Lecture des fichiers scv
-data4936A = loadtxt(open("data/SW_4936_speech_rate_A.csv", "rb"), delimiter="\t", skiprows=1)
-data4936B = loadtxt(open("data/SW_4936_speech_rate_B.csv", "rb"), delimiter="\t", skiprows=1)
+# Lecture des fichiers scv 
+corpus = [{"A":loadtxt(open("data/SW_2001_speech_rate_A.csv", "rb"), delimiter="\t", skiprows=1),
+           "B":loadtxt(open("data/SW_2001_speech_rate_B.csv", "rb"), delimiter="\t", skiprows=1)},
+          {"A":loadtxt(open("data/SW_2005_speech_rate_A.csv", "rb"), delimiter="\t", skiprows=1),
+           "B":loadtxt(open("data/SW_2005_speech_rate_B.csv", "rb"), delimiter="\t", skiprows=1)},
+          {"A":loadtxt(open("data/SW_4936_speech_rate_A.csv", "rb"), delimiter="\t", skiprows=1),
+           "B":loadtxt(open("data/SW_4936_speech_rate_B.csv", "rb"), delimiter="\t", skiprows=1)}]  
 
-# Lissage des données
-def smooth(x,y):
-    b, a = signal.butter(10, 0.1)
-    z = signal.filtfilt(b, a, y)
-    cs = interpolate.CubicSpline(x, z)
-    s = {'x':arange(0, 300, 0.5),
-         'y':cs(arange(0, 300, 0.5))}
-    return s
+# Pour le lissage des données
+def smooth(x,y,window_len):
+    window=hanning(window_len)
+    z=r_[y[window_len-1:0:-1],y,y[-2:-window_len-1:-1]]
+    y2=convolve(window/window.sum(),z,mode='valid')
+    smoothed = interpolate.CubicSpline(linspace(0, int(x[-1]),y2.size), y2)
+    return {'x':arange(0, x[-1],0.5),'y':smoothed(arange(0, x[-1],0.5))}
 
-s4936A = smooth(data4936A[:,2], data4936A[:,1])
-s4936B = smooth(data4936B[:,2], data4936B[:,1])
 
-# Création et affichage du graphique
-plot = figure(width=1000, height=300, y_range=[0,3.5], title="Speech rate discussion 4936")
-plot.line(data4936A[:,2], data4936A[:,1], legend="Speaker A",
-            line_width=1, color="blue", alpha=0.5, line_dash="10 4")
-plot.line(s4936A['x'], s4936A['y'], legend="Smooth Speaker A ",
-            line_width=2, color="blue")
-plot.line(data4936B[:,2], data4936B[:,1], legend="Speaker B",
-            line_width=1, color="red", alpha=0.5, line_dash="10 4")
-plot.line(s4936B['x'], s4936B['y'], legend="Smooth Speaker B",
-            line_width=2, color="red")
-
-show(plot)
-
+grid = []
+# Visualisation des données :
+for discussion in corpus:
+    sA = smooth(discussion["A"][:,2], discussion["A"][:,1],12)
+    sB = smooth(discussion["B"][:,2], discussion["B"][:,1],12)
+        
+    plot = figure(width=800, height=250, y_range=[0,3.5], title="Speech rate")
+    plot.line(discussion["A"][:,2], discussion["A"][:,1], legend="Speaker A",
+                line_width=1, color="blue", alpha=0.5, line_dash="10 4")
+    plot.line(sA['x'], sA['y'], legend="Smooth Speaker A ",
+                line_width=2, color="blue")
+    plot.line(discussion["B"][:,2], discussion["B"][:,1], legend="Speaker B",
+                line_width=1, color="red", alpha=0.5, line_dash="10 4")
+    plot.line(sB['x'], sB['y'], legend="Smooth Speaker B",
+                line_width=2, color="red")
+    grid.append(plot)
+    
+show(gridplot(grid, ncols=1))
